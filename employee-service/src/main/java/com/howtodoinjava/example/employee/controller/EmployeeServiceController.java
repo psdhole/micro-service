3518e385fbe8e84fd77b54b8ce2714fdc99e0d56
Pushparaj Dhole <Pushparaj.Dhole@mobiliya.com>
2019-09-11 18:04:11 +0530

    updated api gate way config

--- employee-service/src/main/java/com/howtodoinjava/example/employee/controller/EmployeeServiceController.java
+++ employee-service/src/main/java/com/howtodoinjava/example/employee/controller/EmployeeServiceController.java
@@ -11,33 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.howtodoinjava.example.employee.beans.Employee;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeServiceController {
-	
-	
 	private static final Map<Integer, Employee> employeeData = new HashMap<Integer, Employee>() {
-		/**
-		 * 
-		 */
 		private static final long serialVersionUID = -3970206781360313502L;
-
 		{
 			put(111,new Employee(111,"Employee1"));
 			put(222,new Employee(222,"Employee2"));
 		}
- 
     };
  
-    @RequestMapping(value = "/findEmployeeDetails/{employeeId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{employeeId}", method = RequestMethod.GET)
     public Employee getEmployeeDetails(@PathVariable int employeeId) {
         System.out.println("Getting Employee details for " + employeeId);
- 
         Employee employee = employeeData.get(employeeId);
         if (employee == null) {
-            
         	employee = new Employee(0, "N/A");
-            
         }
         return employee;
     }
-
 }
