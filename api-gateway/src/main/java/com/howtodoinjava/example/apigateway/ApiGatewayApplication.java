a50651896374cbf8ae77ee8c04ff9c02eb58d659
Pushparaj Dhole <Pushparaj.Dhole@mobiliya.com>
2019-09-13 14:57:18 +0530

    added change for api gate way

--- api-gateway/src/main/java/com/howtodoinjava/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/howtodoinjava/example/apigateway/ApiGatewayApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableZuulProxy
+@EnableZuulServer
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
