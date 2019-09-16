eb1b45d2384743be52790530428481bb9c72aebf
Pushparaj Dhole <Pushparaj.Dhole23@gmail.com>
2019-09-16 12:54:00 +0530

    added zuul routes

--- run-gcp.sh
+++ run-gcp.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-cd employee-service 
-mvn clean install 
-docker build -t gcr.io/striking-shift-248504/employee-service . 
-docker push  gcr.io/striking-shift-248504/employee-service 
-
-cd ../eureka-server 
+cd /home/pushparaj_dhole23/micro-service/eureka-server 
 mvn clean install 
 docker build -t gcr.io/striking-shift-248504/eureka-server . 
 docker push  gcr.io/striking-shift-248504/eureka-server 
+kubectl run  eureka-server  --image=gcr.io/striking-shift-248504/eureka-server  --port=8761
 
-cd ../api-gateway 
+cd /home/pushparaj_dhole23/micro-service/employee-service 
+mvn clean install 
+docker build -t gcr.io/striking-shift-248504/employee-service . 
+docker push  gcr.io/striking-shift-248504/employee-service 
+kubectl run  employee-service   --image=gcr.io/striking-shift-248504/employee-service   --port=8011
+
+
+cd /home/pushparaj_dhole23/micro-service/api-gateway 
 mvn clean install 
 docker build -t gcr.io/striking-shift-248504/api-gateway . 
 docker push  gcr.io/striking-shift-248504/api-gateway 
+kubectl run  api-gateway   --image=gcr.io/striking-shift-248504/api-gateway    --port=8010
