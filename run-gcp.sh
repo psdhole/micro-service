0d50493e4d76dd7e2e15933fd433da9dd5bc7fad
Pushparaj Dhole <Pushparaj.Dhole@mobiliya.com>
2019-09-17 17:44:55 +0530

    updated read me

--- run-gcp.sh
+++ run-gcp.sh
@@ -9,14 +9,15 @@ docker build -t gcr.io/$1/employee-service employee-service/.
 docker build -t gcr.io/$1/api-gateway api-gateway/.
 
 #push all the images to container registry
-docker push  gcr.io/$1/eureka-server
-docker push  gcr.io/$1/employee-service
-docker push  gcr.io/$1/api-gateway
+gcloud auth configure-docker
+docker push gcr.io/$1/eureka-server
+docker push gcr.io/$1/employee-service
+docker push gcr.io/$1/api-gateway
 
 #run the services in kubernates cluster
-kubectl run  eureka-server  --image=gcr.io/$1/eureka-server  --port=8761
-kubectl run  api-gateway   --image=gcr.io/$1/api-gateway --port=8010
-kubectl run  employee-service   --image=gcr.io/$1/employee-service --port=8011
+kubectl run eureka-server --image=gcr.io/$1/eureka-server --port=8761
+kubectl run api-gateway --image=gcr.io/$1/api-gateway --port=8010
+kubectl run employee-service --image=gcr.io/$1/employee-service --port=8011
 
 #expose running service for the public access.
 kubectl expose deployment eureka-server --type=LoadBalancer --port 8761 --target-port 8761
